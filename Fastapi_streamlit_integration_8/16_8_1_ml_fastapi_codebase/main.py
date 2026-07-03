d040a3f5b49e5a6f22c620f5c4f61cebf864c80c
Abhishek Negi <nabhishek971@gmail.com>
2026-07-03 15:25:58 +0530

    Add files via upload

---
+++ Fastapi_streamlit_integration_8/16_8_1_ml_fastapi_codebase/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from predictor import predict
+
+
+app = FastAPI(title="ML Prediction API")
+
+# define input schema
+class PredictionInput(BaseModel):
+    Pregnancies: int
+    Glucose: float
+    BloodPressure: float
+    SkinThickness: float
+    Insulin: float
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: int
+
+
+# ml prediction endpoint
+@app.post("/predict")
+def predict_diabetes(input_data: PredictionInput):
+    prediction = predict(input_data.model_dump())
+    return {
+        "Prediction": int(prediction)
+    }
+
+# RUN=> uvicorn main:app
+# automatically restart the server whenever you save code changes=> uvicorn main:app --reload
+# To access the interactive documentation, start your application and navigate to http://127.0.0.1:8000/docs in your browser.
+
+
+
+
+
